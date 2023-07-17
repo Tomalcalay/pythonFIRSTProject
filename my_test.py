6c821c272e2e15a454033b40a15fc2a54fdd6224
Tomalcalay <tomalcalay2611@gmail.com>
2023-07-17 17:25:30 +0300

    all the project, tests and bonuses

---
+++ my_test.py
@@ -0,0 +1,20 @@
+import unittest
+import mainProject
+
+class testadd(unittest.TestCase):
+    def test_value(self):
+        from mainProject import get_user_value
+        assert get_user_value()
+
+    def test_result(self):
+        import projectClassILS
+        result = projectClassILS.ILS.calculate(5)
+        assert result == 1.4000000000000001
+
+    def test_file(self):
+        list_results = mainProject.third_screen(5)
+        results_file = open('C:/Users/nadav2022/PycharmProjects/pythonFIRSTProject/results_list.txt', 'r')
+        content = results_file.read()
+        assert content == str(list_results)
+        results_file.close()
+
